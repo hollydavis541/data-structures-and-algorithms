c5abfcab870729096bf5906c79c1a119adeccf85
Holly Davis <hollydavis541@gmail.com>
2019-10-30 19:06:31 -0700

    challenge 01 complete

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -7,7 +7,8 @@ Write a function named sortBackwards that takes in an array of numbers and retur
 ------------------------------------------------------------------------------------------------ */
 
 const sortBackwards = (arr) => {
-  // Solution code here...
+  arr.sort((a, b) => b - a);
+  return arr
 };
 
 /* ------------------------------------------------------------------------------------------------
