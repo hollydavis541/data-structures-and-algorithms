812b2d5250c793d356b52a7fb47221a11b65ab0b
Holly Davis <hollydavis541@gmail.com>
2019-11-02 17:06:41 -0700

    testing

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -70,7 +70,7 @@ Here is an example of the input:
 
 const sortByPrice = (arr) => {
   return arr.sort((a, b) => a.price - b.price)
-};
+}; 
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
