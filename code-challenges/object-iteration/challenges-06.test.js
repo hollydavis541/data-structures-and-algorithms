6a5fdd7215d15a629567f522cdeb9b0aab93f549
Holly Davis <hollydavis541@gmail.com>
2019-11-04 20:41:06 -0800

    challenge 02 complete

--- code-challenges/object-iteration/challenges-06.test.js
+++ code-challenges/object-iteration/challenges-06.test.js
@@ -70,7 +70,9 @@ let characters = [
 
 const getHouses = (arr) => {
   let houses = [];
-  // Solution code here...
+  Object.values(arr).forEach( value => {
+    houses.push(value.house)
+  })
   return houses;
 };
 
